b017f3b90c5044d134ec6c18a5fce237feb656ac
Tommy <tommymanuel08@gmail.com>
2025-07-07 23:55:27 -0400

    Version 7 cleaned up the code, got a suggestion to add more error handling and exits. As well as corrected some of my loops since they were redudant some of them. The functions as well were updated the gettingfilenames since it could have been used more simply. Although fairly certain my old function worked so switched it mainly for visibility to be honest.

--- main.py
+++ main.py
@@ -24,15 +24,12 @@ def filter(banned,file,filtered_file,):
     return filtered_file
 
 def getting_filenames(title):
-    index = title.rfind(".") + 1
-    word = ""
-    while index < len(title):
-        word += title[index]
-        index += 1
-    return word
+    ext = os.path.splitext(title)[-1]
+    if ext:
+        return ext[1:].lower()
+    else:
+        return ''
         
-        
-            
 
 if __name__ == "__main__":
     
@@ -49,7 +46,7 @@ if __name__ == "__main__":
     "Cookies", "Local Settings", "PrintHood", "Recent", "Searches",
     "SendTo", "AppData", "Application Data", "NetHood", "Start Menu",
     "NTUSER.DAT", "NTUSER.DAT.LOG1", "NTUSER.DAT.LOG2", "thumbs.db",
-    "desktop.ini", "$Recycle.Bin", "System Volume Information","All Users","Default User","desktop.ini"
+    "desktop.ini", "$Recycle.Bin", "System Volume Information","All Users","Default User",
 
     # macOS-specific (non-dot) system folders
     "TemporaryItems", "VolumeIcon.icns",
@@ -63,128 +60,171 @@ if __name__ == "__main__":
     filter(banned,user_folder,filtered_file)
     
     list_user_folder = "\n".join(f"{i}. {pair}" for i, pair in enumerate(filtered_file)) #organinizes the folders with there corresponding number call
-    
-    while True:
-        try:
-            command = int(input(f"\nHere is the list, please choose one with its corresponding number to keep going:\n\n{list_user_folder}\n"))
-            if filtered_file[command] in filtered_file:
-                break
-            
-        except IndexError:
-            print("\nThat value is out of range. Please try again.\n")
-            True
-            
-        except ValueError:
-            print("\nInvalid input. Please enter a valid number to continue.")
-            True
-            
-    print("\nYour choice is valid. Let's continue.\n")
-    
-    #move into the next folder
-    temp_path = os.path.join(path,filtered_file[command])
-    
-    #list all information in this path
-    all_info = os.listdir(temp_path)
-    
-    #we are getting all the folders in this directory and removing any hidden files 
-    all_folders_in_this_directory = []
-    getting_folders(temp_path,all_folders_in_this_directory,all_info)
-    filtered_folders = []
-    filter(banned,all_folders_in_this_directory,filtered_folders)
-    
-    #we basically got to ask the user to pick which folder to organize again inside the other folder
-    list_user_folder2 = "\n".join(f"{i}. {pair}" for i, pair in enumerate(filtered_folders)) #organinizes the folders with there corresponding number call inside another directory
-    
-    while True:
-        try:
-            command2 = int(input(f"Here is the list, please choose one with its corresponding number to organize:\n\n{list_user_folder2}\n"))
-            if filtered_folders[command2] in filtered_folders:
-                break
-            
-        except IndexError:
-            print("\nThat value is out of range. Please try again.\n")
-            True
-            
-        except ValueError:
-            print("\nInvalid input. Please enter a valid number to continue.")
-            True
-            
-    print("\nYour choice is valid. Let's continue.\n")
-    
-    #We are going to do a final scan in the final scan for folders
-    
-    temp_path2 = os.path.join(temp_path,filtered_folders[command2])
-
-    
-    final_folders_unorganized = os.listdir(temp_path2)
-    final_folders = []
-    getting_folders(temp_path2, final_folders, final_folders_unorganized)
-    
-    #final files all the files in this directory
-    final_files = []
-    getting_files(temp_path2,final_files)
-    
-    #filtered_folders has all the folders 1st layer temp1 #final_folders has the 2st layer folder temp2
-    total_folders1 = filtered_folders + final_folders
-    
-    predefined_folders = {
-    "images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".pdf"],
-    "videos": [".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv"],
-    "audio": [".mp3", ".wav", ".flac", ".aac", ".ogg"],
-    "documents": [".doc", ".docx", ".odt", ".rtf"],
-    "spreadsheets": [".xls", ".xlsx", ".ods", ".csv"],
-    "presentations": [".ppt", ".pptx", ".odp"],
-    "textfiles": [".txt", ".md"],
-    "executables": [".exe", ".msi", ".sh", ".bat", ".app"],
-    "archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
-    "scripts": [".py", ".js", ".java", ".cpp", ".html", ".css"],
-    "Other_Folders": []}
-    
-    total_folders2 = []
-    categorie_key = []
-    
-    for categories, extension_files in predefined_folders.items():
-        os.makedirs(
-        os.path.join(temp_path,categories),
-        exist_ok=True)
+    final = 1
+    while final != 0:
         
-        for extension in extension_files:
-            total_folders2.append(extension.replace(".",""))
-            os.makedirs(
-            os.path.join(temp_path,categories,extension.replace(".","")),
-            exist_ok=True)
+        while True:
+            try:
+                command = int(input(f"\nHere is the list, please choose one with its corresponding number to keep going:\n\n{list_user_folder}\n"))
+                
+                if command >= len(filtered_file):
+                    print("\nThat value is out of range. Please try again.\n")
+                    continue
+                
+                if command <= -1:
+                    print("\nTo exit, type -1\n")
+                    exit()
+                    
+                else:
+                    break
+                
+            except ValueError:
+                print("\nInvalid input. Please enter a valid number to continue.")
+                    
+                
+        print("\nYour choice is valid. Let's continue.\n")
+        
+        
+        #move into the next folder
+        temp_path = os.path.join(path,filtered_file[command])
+        
+        #list all information in this path
+        all_info = os.listdir(temp_path)
+        
+        #we are getting all the folders in this directory and removing any hidden files 
+        all_folders_in_this_directory = []
+        getting_folders(temp_path,all_folders_in_this_directory,all_info)
+        filtered_folders = []
+        filter(banned,all_folders_in_this_directory,filtered_folders)
+        
+        #we basically got to ask the user to pick which folder to organize again inside the other folder
+        list_user_folder2 = "\n".join(f"{i}. {pair}" for i, pair in enumerate(filtered_folders)) #organinizes the folders with there corresponding number call inside another directory
+        
     
-    for folders_to_move_path1 in final_folders:
-        for folder in folders_to_move_path1:
-            shutil.move(
+        
+        while True:
+            try:
+                command2 = int(input(f"\nHere is the list, please choose one with its corresponding number to keep going:\n\n{list_user_folder2}\n"))
+                
+                if command2 <= -1:
+                    print("\nTo exit, type -1\n")
+                    exit()
+                    
+                elif not filtered_folders:
+                    print("\nNo subfolders found to organize.\n")
+                    continue
+                    
+                    
+                elif command2 >= len(filtered_folders):
+                    print("\nThat value is out of range. Please try again.\n")
+                    
+                else:
+                    break
+                
+            except ValueError:
+                print("\nInvalid input. Please enter a valid number to continue.")
+                    
+        print("\nYour choice is valid. Let's continue.\n")
+        
+        #We are going to do a final scan in the final scan for folders
+        
+        temp_path2 = os.path.join(temp_path,filtered_folders[command2])
+
+        
+        final_folders_unorganized = os.listdir(temp_path2)
+        final_folders = []
+        getting_folders(temp_path2, final_folders, final_folders_unorganized)
+        
+        #final files all the files in this directory
+        final_files = []
+        getting_files(temp_path2,final_files)
+        
+        #filtered_folders has all the folders 1st layer temp1 #final_folders has the 2st layer folder temp2
+        total_folders1 = filtered_folders + final_folders
+        
+        predefined_folders = {
+        "images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".pdf"],
+        "videos": [".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv"],
+        "audio": [".mp3", ".wav", ".flac", ".aac", ".ogg"],
+        "documents": [".doc", ".docx", ".odt", ".rtf"],
+        "spreadsheets": [".xls", ".xlsx", ".ods", ".csv"],
+        "presentations": [".ppt", ".pptx", ".odp"],
+        "textfiles": [".txt", ".md"],
+        "executables": [".exe", ".msi", ".sh", ".bat", ".app"],
+        "archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
+        "scripts": [".py", ".js", ".java", ".cpp", ".html", ".css"],
+        "Other_Folders": []}
+        
+        total_folders2 = []
+        categorie_key = []
+        
+        for categories, extension_files in predefined_folders.items():
+            os.makedirs(
+            os.path.join(temp_path,categories.lower()),
+            exist_ok=True)
+            
+            for extension in extension_files:
+                total_folders2.append(extension.replace(".",""))
+                os.makedirs(
+                os.path.join(temp_path,categories.lower(),extension.replace(".","").lower()),
+                exist_ok=True)
+        
+        for folder in final_folders:
+            try:
+                shutil.move(
                 os.path.join(temp_path2,folder),
                 os.path.join(temp_path,"Other_Folders"))
-    
-    for categories, extension_files in predefined_folders.items():
-        categorie_key.append(categories)
-        for extension in extension_files:
-            total_folders2.append(extension.replace(".",""))
-            
-    total_folders3 = total_folders1 + total_folders2 + categorie_key
-    #adds new files not already in folders into folders as well as creates folders that are not established
-    
-    
-    for file in final_files:
+            except shutil.Error as e:
+                print(f"Could not move{os.path.join(temp_path2,folder)} to {os.path.join(temp_path,"Other_Folders")}:{e}")
         
-        if "." in file and file not in total_folders3:
-            word = getting_filenames(file)
-            os.makedirs(os.path.join(temp_path,word),exist_ok=True)
-            shutil.move(os.path.join(temp_path2, file),os.path.join(temp_path, word))
+        for categories, extension_files in predefined_folders.items():
+            categorie_key.append(categories)
+            for extension in extension_files:
+                total_folders2.append(extension.replace(".",""))
+                
+        total_folders3 = total_folders1 + total_folders2 + categorie_key
+        #adds new files not already in folders into folders as well as creates folders that are not established
+        
+        
+        for file in final_files:
             
-        elif "." in file and file in total_folders3:
-            if getting_filenames(file).lower() in total_folders2:
-                target = "." + getting_filenames(file)
+            if "." in file and file not in total_folders3:
+                word = getting_filenames(file).lower()
+                os.makedirs(os.path.join(temp_path,word),exist_ok=True)
+                
+                try:
+                    shutil.move(os.path.join(temp_path2, file),os.path.join(temp_path, word))
+                except shutil.Error as e:
+                            print(f"Could not move{os.path.join(temp_path2,file)} to {os.path.join(temp_path, word)}:{e}")
+                
+            elif "." in file and getting_filenames(file).lower() in total_folders2:
+                target = "." + getting_filenames(file).lower()
                 for categorie, extension in predefined_folders.items():
                     if target in extension:
-                        word = getting_filenames(file)
-                        shutil.move(
-                        os.path.join(temp_path2,file),
-                        os.path.join(temp_path,categorie,word))
-        else:
-            shutil.move(os.path.join(temp_path2,file),os.path.join(temp_path,categories,extension))
-            
+                        word = getting_filenames(file).lower()
+                        
+                        
+                        try:
+                            shutil.move(
+                            os.path.join(temp_path2,file),
+                            os.path.join(temp_path,categorie.lower(),word))
+                        except shutil.Error as e:
+                            print(f"Could not move{os.path.join(temp_path2,file)} to {os.path.join(temp_path,categorie.lower(),word)}:{e}")
+            else:
+                try:
+                    shutil.move(os.path.join(temp_path2,file),os.path.join(temp_path,'Other_Folders'))
+                except shutil.Error as e:
+                            print(f"Could not move{os.path.join(temp_path2,file)} to {os.path.join(temp_path,'Other_Folders')}:{e}")
+                
+        while True:
+            final = int(input("\nWould you like to organize a different folder?\n"
+                "0: Continue with current folder\n"
+                "1: Choose a different User folder\n"
+                "Enter your choice (0, 1): "))
+            if final in (0,1):
+                break
+            else:
+                print("\nInvalid input. Please enter a valid number to continue.")
+                
+    print("Goodbye Have a nice day")
+                    
